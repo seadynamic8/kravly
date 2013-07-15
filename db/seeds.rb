d34478f0de9a5a8d37a77957c7f59aa82ccd3c6c
Jarvis <seadynamic8@zoho.com>
2013-07-15 15:42:37 -0400

    Seeded database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Users.delete_all
+#Users.delete_all
 users = [
-	{ }
-
-]
+	{ firstname: "Jarvis", lastname: "Kwan", username: "jkwan", email: "jkwan@example.com" },
+	{ firstname: "Jovita", lastname: "Lai", username: "jlai", email: "jlai@example.com" },
+	{ firstname: "Charles", lastname: "Lai", username: "clai", email: "clai@example.com" },
+	{ firstname: "Jenny", lastname: "Yu", username: "jyyu", email: "jyyu@example.com" },
+].each do |user|
+	User.where(username: user[:username]).first_or_create!(user)
+end
