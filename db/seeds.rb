a69ba4ff12106b61c00ad52928740040aa4459a6
Jarvis <seadynamic8@zoho.com>
2013-11-26 18:09:10 -0500

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'faker'
+# require 'faker'
 
 #Idea.delete_all
 # ideas = [
